099f6164fb902a675ecf29cac3904d1dd3ff98a2
Bendik <knaben@OBOS.NO>
2020-01-23 13:38:40 +0100

    added button class

--- gui.py
+++ gui.py
@@ -1,5 +1,21 @@
 from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+from kivy.lang import Builder
+from os import listdir 
+
+
+kv_path = './kv/'
+for kv in listdir(kv_path):
+    Builder.load_file(kv_path+kv)
+
+
+class AddButton(Button):
+    pass
+
+
+class SubtractButton(Button):
+    pass
 
 
 class Container(GridLayout):

8e3ee094625e01958770933cd52323f3e9c12c52
Bendik Knapstad <b.knapstad@gmail.com>
2020-01-24 16:54:33 +0100

    update window title

--- gui.py
+++ gui.py
@@ -12,7 +12,7 @@ class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         
-        self.setWindowTitle("My Awesome App")
+        self.setWindowTitle("Finn urler -  OBOS")
         self.left = 500
         self.top = 500
 

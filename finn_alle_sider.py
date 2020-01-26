ff14e8399596856b8467827cfb6524fea79703fd
Bendik Knapstad <b.knapstad@gmail.com>
2020-01-26 21:24:22 +0100

    add black formatting

--- finn_alle_sider.py
+++ finn_alle_sider.py
@@ -10,12 +10,17 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
 def get_sitemap() -> BS:
-    sitemaps = ["https://www.obos.no/sitemaps/sitemap_obos.no.xml", "http://www.obos.no/sitemap.xml","https://bank.obos.no/sitemap.xml"]
-    sitemap=""
+    sitemaps = [
+        "https://www.obos.no/sitemaps/sitemap_obos.no.xml",
+        "http://www.obos.no/sitemap.xml",
+        "https://bank.obos.no/sitemap.xml",
+    ]
+    sitemap = ""
     for i in sitemaps:
-        tekst = requests.get(i,verify=False).text
-        sitemap+=tekst
+        tekst = requests.get(i, verify=False).text
+        sitemap += tekst
     return BS(sitemap, "xml")
+
 
 def find_urlfragment(tag: str, sitemap: BS) -> list:
     locs = sitemap.findAll("loc")
@@ -25,27 +30,29 @@ def find_urlfragment(tag: str, sitemap: BS) -> list:
         urls = [url.text for url in locs if tag.lower() in url.text.lower()]
     return urls
 
+
 def get_titles(urls: list):
     titles = []
     for url in urls:
         response = requests.get(url)
-        soup= BS(response.text)
+        soup = BS(response.text)
         titles.append(soup.title.text)
     return titles
+
 
 def get_title(soup: BS):
     try:
         return soup.title.text
     except Exception:
-        return "No title found" 
+        return "No title found"
 
 
 if __name__ == "__main__":
     if not sys.argv[1]:
         print("Add a url fragment ex: /bedrift ")
-        
+
     else:
-        url = sys.argv [1]
+        url = sys.argv[1]
 
         urls = find_urlfragment(url, get_sitemap())
         with open(f"{url}.json", "w") as file:
